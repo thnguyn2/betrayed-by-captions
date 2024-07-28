87ae75df3d8ef33a25f952bd0b16baca2a062b52
Tan Nguyen <tan.nguyen@pathai.com>
2024-07-28 23:55:00 +0000

    Add debugging config

--- configs/instance/path_ground_baseline_1_sample.py
+++ configs/instance/path_ground_baseline_1_sample.py
@@ -9,11 +9,11 @@ num_unknown_classes = 0
 num_classes = num_things_classes + num_stuff_classes
 num_known_classes = num_classes - num_unknown_classes
 
-known_file = f'../datasets/unknown/path_ground_known_{num_classes}.txt'
-unknown_file = None
-class_to_emb_file = f'../datasets/embeddings/quilt_class_with_pubmed_bert_emb_ordered.json'
+known_file = f'./datasets/unknown/path_ground_known_{num_classes}.txt'
+unknown_file = None  # Don't use an empty file, it will create a class name of ''
+class_to_emb_file = f'./datasets/embeddings/quilt_class_with_pubmed_bert_emb_ordered.json'
 embeding_type = 'pubmed-bert'
-init_path = './pretrained/class_ag_pretrained_3x.pth'  # From class agnostic pretraining  # Class agnostic pretraining
+init_path = './work_dirs/path_ground_baseline_1_sample/epoch_65.pth'  # From class agnostic pretraining  # Class agnostic pretraining
 
 model = dict(
     type='Mask2FormerOpen',  # Name of the model
@@ -154,7 +154,6 @@ model = dict(
         text_emb_norm=True,
         caption_emb_type=embeding_type,
         caption_gen_emb_type=embeding_type),
-    
     panoptic_fusion_head=dict(
         type='MaskFormerFusionHeadOpen',
         num_things_classes=num_classes,
@@ -253,45 +252,45 @@ data = dict(
         ann_file=data_root + 'annotations_region_only/train_instances.json',
         caption_ann_file=data_root + 'annotations_region_only/train_captions.json',
         img_prefix=data_root + 'images/',
-        pipeline=train_pipeline,
+        transform_pipeline=train_pipeline,
         
         filter_empty_gt=False,
         known_file=known_file,
         unknown_file=unknown_file,
         class_agnostic=False,
         emb_type=embeding_type,
-        use_reduced_size_dataset=False,
+        use_reduced_size_dataset=True,
         class_to_emb_file=class_to_emb_file,
         ),
     
     val=dict(
         type=dataset_type,
-        ann_file=data_root + 'annotations_region_only/val_instances.json',
-        caption_ann_file=data_root + 'annotations_region_only/val_captions.json',
+        ann_file=data_root + 'annotations_region_only/train_instances.json',
+        caption_ann_file=data_root + 'annotations_region_only/train_captions.json',
         
         img_prefix=data_root + 'images/',
-        pipeline=test_pipeline,
+        transform_pipeline=test_pipeline,
         
         known_file=known_file,
         unknown_file=unknown_file,
         class_agnostic=False,
         eval_types=['all_results'],
-        use_reduced_size_dataset=False,    
+        use_reduced_size_dataset=True,    
         class_to_emb_file=class_to_emb_file,
     ),
     
     test=dict(
         type=dataset_type,
-        ann_file=data_root + 'annotations_region_only/val_instances.json',
-        caption_ann_file=data_root + 'annotations_region_only/val_captions.json',
+        ann_file=data_root + 'annotations_region_only/train_instances.json',
+        caption_ann_file=data_root + 'annotations_region_only/train_captions.json',
         img_prefix=data_root + 'images/',
-        pipeline=test_pipeline,
+        transform_pipeline=test_pipeline,
         
         known_file=known_file,
         unknown_file=unknown_file,
         class_agnostic=False,
         eval_types=['all_results'],
-        use_reduced_size_dataset=False,
+        use_reduced_size_dataset=True,
         class_to_emb_file=class_to_emb_file,
         ),
     )
@@ -327,11 +326,11 @@ lr_config = dict(
 
 runner = dict(
     type='EpochBasedRunner', 
-    max_epochs=1000,
+    max_epochs=10000,
 )
 
 log_config = dict(
-    interval=20,  # in the unit of iters, #iters = #images total / (mini batch size) * epoches
+    interval=1,  # in the unit of iters, #iters = #images total / (mini batch size) * epoches
     hooks=[
         dict(type='TextLoggerHook', by_epoch=False),
         dict(type='TensorboardLoggerHook', by_epoch=False)
@@ -339,7 +338,7 @@ log_config = dict(
 workflow = [('train', 1)]
 
 checkpoint_config = dict(
-    by_epoch=True, interval=50, save_last=True, max_keep_ckpts=2) 
+    by_epoch=True, interval=20, save_last=True, max_keep_ckpts=3) 
 
 evaluation = dict(
     interval=20,  # in the unit of epochs.
--- open_set/datasets/coco_open.py
+++ open_set/datasets/coco_open.py
@@ -32,7 +32,7 @@ from mmdet.datasets.custom import CustomDataset
 from .utils.parser import LVISParser, NLTKParser, ImageNet21KParser, MedLVISParser
 from open_set.models.utils.bert_embeddings import BERT_MODEL_BY_EMBEDDING_TYPES
 
-_LIMITED_DATASET_SIZE = 120
+_LIMITED_DATASET_SIZE = 1
 @DATASETS.register_module()
 class CocoDatasetOpen(CustomDataset):
 
